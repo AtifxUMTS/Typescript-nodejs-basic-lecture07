73ce78efc2b3527f83d55e55b97e901647f1185a
AtifxUMTS <atifalishahnaqvi3345@gmail.com>
2023-09-23 19:50:31 -0700

    first commit

---
+++ lecture07/objects.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let teacher = {
+    name: "Zeeshan",
+    experience: "10"
+};
+console.log(teacher.name);
+console.log(teacher["experience"]);
---
+++ lecture07/objects.ts
@@ -0,0 +1,46 @@
+let teacher = {
+    name: "Zeeshan",
+    experience: "10"
+}
+
+console.log(teacher.name);
+console.log(teacher["experience"]);
+
+
+// Type Declaration
+let student : {
+    name: string,
+    age: number
+}
+
+student = {
+    name: "Atif Ali Shah",
+    age: 30
+}
+
+console.log(student["name"]);
+console.log(student.age);
+//Example 3
+function greet(person: { name: string; age: number }) {
+    return "Hello " + person.name;
+  }
+  //Example 4
+  interface Person {
+    name: string;
+    age: number;
+  }
+   
+  function greet1(person: Person) {
+    return "Hello " + person.name;
+  }
+  //Example 5
+  //or they can be named by using either an interface:
+
+interface Person {
+  name: string;
+  age: number;
+}
+ 
+function greet2(person: Person) {
+  return "Hello " + person.name;
+}
